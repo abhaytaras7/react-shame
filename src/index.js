9d6ccb689343c22ef02e36fef97261cdc6cd321b
ABHAY TARAS <73850631+abhaytaras7@users.noreply.github.com>
2022-11-02 23:55:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import './style.css';
 import Sauce from './app.js';
 
 
-ReactDom.render(<Sauce/>, document.getElementById('root'))
+ReactDom.render(<Sauce/>, document.getElementById('root')) 
+
+let xLogs = new Date();
